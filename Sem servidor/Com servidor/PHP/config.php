3e15e16f142b2e617ada3856af517edef645975d
Welder Claudio <111553376+elderScrols@users.noreply.github.com>
2023-12-27 22:20:06 -0300

    Delete Sem servidor directory

--- Sem servidor/Com servidor/PHP/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    define('HOST', 'localhost');
-    define('USER', 'root');
-    define('PASS', '');
-    define('BASE', 'console');
-
-    $conn = new MySQLi(HOST, USER, PASS, BASE);
-?>
-
